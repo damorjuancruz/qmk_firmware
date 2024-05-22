41993f4bcd3019a5014d0c84a9c09380a536e958
Juan Cruz D'Amor <damorjuancruz@gmail.com>
2024-05-22 20:09:07 -0300

    go back to qwerty and improve valorant layer

--- keyboards/voyager/keymaps/damorjuancruz/keymap.c
+++ keyboards/voyager/keymaps/damorjuancruz/keymap.c
@@ -35,17 +35,17 @@ enum custom_keycodes {
 // clang-format off
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_voyager(
-        _______, _______      , _______      , _______      , _______        , _______         ,                   _______      , _______       , _______      , _______      , _______      , _______ ,
-        KC_TAB , KC_B         , L_RING2(KC_Y), KC_O         , KC_U           , KC_QUOTE        ,                   KC_SCLN      , KC_L          , KC_D         , R_RING2(KC_W), KC_V         , KC_Z    ,
-        CW_TOGG, L_PINKY(KC_C), L_RING1(KC_I), L_MIDDL(KC_E), L_INDEX(KC_A)  , KC_COMMA        ,                   KC_DOT       , R_INDEX(KC_H) , R_MIDDL(KC_T), R_RING1(KC_S), R_PINKY(KC_N), KC_Q    ,
-        _______, KC_G         , KC_X         , KC_J         , KC_K           , KC_MINUS        ,                   KC_SLASH     , KC_R          , KC_M         , KC_F         , KC_P         , KC_ENTER,
+        _______, _______      , _______      , _______      , _______        , _______         ,                   _______      , _______       , _______      , _______      , _______         , _______ ,
+        KC_TAB , KC_Q         , L_RING2(KC_W), KC_E         , KC_R           , KC_T            ,                   KC_Y         , KC_U          , KC_I         , R_RING2(KC_O), KC_P            , KC_MINS ,
+        CW_TOGG, L_PINKY(KC_A), L_RING1(KC_S), L_MIDDL(KC_D), L_INDEX(KC_F)  , KC_G            ,                   KC_H         , R_INDEX(KC_J) , R_MIDDL(KC_K), R_RING1(KC_L), R_PINKY(KC_SCLN), KC_QUOT ,
+        _______, KC_Z         , KC_X         , KC_C         , KC_V           , KC_B            ,                   KC_N         , KC_M          , KC_COMM      , KC_DOT       , KC_SLSH         , KC_ENTER,
                                                               LT(1, KC_SPACE), LT(3, KC_ESCAPE),                   OSM(MOD_LSFT), LT(2, KC_BSPC)
     ),
     [1] = LAYOUT_voyager(
-        _______, _______      , _______      , _______      , _______      , _______,                   _______ , _______      , _______      , _______      , _______      , _______,
-        _______, KC_DOT       , L_RING2(KC_Y), KC_LPRN      , KC_RPRN      , KC_PERC,                   KC_COLN , KC_PLUS      , KC_MINUS     , R_RING2(KC_D), KC_W         , XXXXXXX,
-        _______, L_PINKY(KC_6), L_RING1(KC_3), L_MIDDL(KC_1), L_INDEX(KC_2), KC_7   ,                   KC_9    , R_INDEX(KC_4), R_MIDDL(KC_0), R_RING1(KC_5), R_PINKY(KC_8), XXXXXXX,
-        _______, RSFT(KC_G)   , KC_AT        , KC_J         , KC_K         , KC_DLR ,                   KC_SLASH, KC_ASTR      , KC_EQUAL     , KC_LABK      , KC_RABK      , _______,
+        _______, _______      , _______      , _______      , _______      , _______,                   _______ , _______      , _______      , _______          , _______      , _______,
+        _______, KC_DOT       , L_RING2(KC_Y), KC_LPRN      , KC_RPRN      , KC_PERC,                   KC_COLN , KC_PLUS      , KC_MINUS     , R_RING2(KC_EQUAL), KC_W         , XXXXXXX,
+        _______, L_PINKY(KC_6), L_RING1(KC_3), L_MIDDL(KC_1), L_INDEX(KC_2), KC_7   ,                   KC_9    , R_INDEX(KC_4), R_MIDDL(KC_0), R_RING1(KC_5)    , R_PINKY(KC_8), XXXXXXX,
+        _______, RSFT(KC_G)   , KC_AT        , KC_J         , KC_K         , KC_DLR ,                   _______ , KC_ASTR      , KC_SLASH     , KC_LABK      , KC_RABK      , _______,
                                                               _______      , XXXXXXX,                   KC_SPACE, KC_BSPC
     ),
     [2] = LAYOUT_voyager(
@@ -57,17 +57,17 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [3] = LAYOUT_voyager(
         RGB_TOG, RGB_MOD, RGB_VAD, RGB_VAI, EE_CLR , _______,                   _______, _______, _______, _______, _______, QK_BOOT,
-        _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______,                   KC_PGUP, KC_HOME, KC_UP  , KC_END , _______, _______,
-        _______, KC_MPRV, KC_MNXT, KC_MSTP, KC_MPLY, _______,                   KC_PGDN, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______,
+        _______, _______, KC_VOLD, KC_VOLU, KC_MUTE, _______,                   KC_HOME, KC_PGDN, KC_PGUP, KC_END , _______, _______,
+        _______, KC_MPRV, KC_MNXT, KC_MSTP, KC_MPLY, _______,                   KC_LEFT, KC_DOWN, KC_UP  , KC_RGHT, _______, _______,
         _______, _______, _______, HSV_0_255_255, HSV_74_255_255, HSV_169_255_255, _______, LCTL(LSFT(KC_TAB)), LCTL(KC_TAB), _______, _______, _______,
                                             _______, _______,                   _______, _______
     ),
     [4] = LAYOUT_voyager(
-        KC_GRV , KC_1, KC_2, KC_3, KC_4  , KC_5   ,                   KC_6   , KC_7   , KC_8   , KC_9  , KC_0   , KC_MINS,
-        KC_TAB , KC_Q, KC_W, KC_E, KC_R  , KC_T   ,                   KC_Y   , KC_U   , KC_I   , KC_O  , KC_P   , KC_RBRC,
-        KC_LSFT, KC_A, KC_S, KC_D, KC_F  , KC_G   ,                   KC_H   , KC_J   , KC_K   , KC_L  , KC_COLN, KC_QUOT,
-        KC_LCTL, KC_Z, KC_X, KC_C, KC_V  , KC_B   ,                   KC_N   , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_RSFT,
-                                   KC_SPC, KC_LALT,                   KC_LCTL, KC_BSPC
+        _______, KC_CAPS, KC_1, KC_2, KC_3  , KC_4   ,                   KC_6     , KC_7   , KC_8   , KC_9  , KC_0   , KC_MINS ,
+        KC_T   , KC_TAB , KC_Q, KC_W, KC_E  , KC_R   ,                   KC_Y     , KC_U   , KC_F5  , KC_F6 , KC_P   , KC_RBRC ,
+        KC_G   , KC_LSFT, KC_A, KC_S, KC_D  , KC_F   ,                   KC_H     , KC_J   , KC_K   , KC_L  , KC_COLN, KC_QUOT ,
+        KC_B   , KC_LCTL, KC_Z, KC_X, KC_C  , KC_V   ,                   KC_N     , KC_M   , KC_COMM, KC_DOT, KC_SLSH, KC_ENTER,
+                                      KC_SPC, KC_LALT,                   KC_ESCAPE, KC_BSPC
     ),
 };
 
